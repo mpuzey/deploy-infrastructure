9fff3e667cbe355e002da2465e59a7f12317984f
matthew.puzey <matthew.puzey@nhs.net>
2017-11-27 22:05:00 +0000

    [DEV-0111] - Wrote noddy terraform init script for loading config and later extend to support different use cases

--- terraform-spike/terraform-init.py
+++ terraform-spike/terraform-init.py
@@ -1,22 +1,32 @@
 """The purpose of this script it to wrap terraform init to manage s3 keys under the Terraform remote state bucket."""
-import argparse, sys
+import argparse
+import subprocess
 
-parser=argparse.ArgumentParser()
+parser = argparse.ArgumentParser()
 
-parser.add_argument('--project', help='The name of the project e.g. azcard')
-parser.add_argument('--account_id', help='The aws account id')
-parser.add_argument('--region', help='The aws region e.g. eu-west-2')
-parser.add_argument('--environment', help='The aws environment e.g. nonprod')
-parser.add_argument('--component', help='The terraform component to name the tf state file by')
+parser.add_argument('--project', help='The name of the project e.g. azcard', required=True)
+parser.add_argument('--account_id', help='The aws account id', required=True)
+parser.add_argument('--region', help='The aws region e.g. eu-west-2', required=True)
+parser.add_argument('--environment', help='The aws environment e.g. nonprod', required=True)
+parser.add_argument('--component', help='The terraform component to name the tf state file by', required=True)
 
-args=parser.parse_args()
-
-print(args)
+args = parser.parse_args()
 
 backend_config_file = open('backend-config.tf', 'r')
 
-file_contents = backend_config_file.read()
+file_contents = backend_config_file \
+    .read() \
+    .replace('{project}', args.project) \
+    .replace('{account_id}', args.account_id) \
+    .replace('{region}', args.region) \
+    .replace('{environment}', args.environment) \
+    .replace('{component}', args.component) \
+    . replace(" ", "") \
+    .splitlines()
 
-# file_contents.replace()
+init_command = ["terraform", "init"]
 
-# TODO: called terraform init with the args
+for line in file_contents:
+    init_command.append("-backend-config=%s" % line)
+
+subprocess.run(init_command)
