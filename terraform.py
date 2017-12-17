56c9c7052da2c04c7ac16aa4b764f0b85f55e830
Alistair Webster <alistair.webster@nhs.net>
2017-12-17 16:14:11 +0000

    [DEV-0013]- Refactor python terraform script to allow tf vars in order to create prod lambda IAM role

--- terraform.py
+++ terraform.py
@@ -7,18 +7,34 @@ import os
 BASE_PATH = os.path.dirname(os.path.realpath(__file__))
 
 
-def execute(cmd, path, arguments):
+def handle():
 
-    # This env var provides a workaround for the following issue:
-    # https://github.com/hashicorp/terraform/issues/13589
-    env_vars = dict(os.environ, AWS_PROFILE=arguments.project)
+    args, tf_vars = parse_arguments()
+    validate_component(args)
+    component_path = os.path.join(BASE_PATH, 'components', args.component)
+    init(args, component_path)
+    action(args, component_path, tf_vars)
 
-    popen = subprocess.Popen(cmd,
-                             universal_newlines=True,
-                             cwd=path,
-                             env=env_vars)
 
-    popen.communicate()
+def parse_arguments():
+
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('--action', help='The Terraform action to perform e.g. plan', required=True)
+    parser.add_argument('--project', help='The name of the project default=azcard', default='azcard')
+    parser.add_argument('--profile', help='The name of the AWS profile default=azcard', default='azcard')
+    parser.add_argument('--region', help='The aws region default=eu-west-2', default='eu-west-2')
+    parser.add_argument('--environment', help='The aws environment e.g. test', required=True)
+    parser.add_argument('--component', help='The terraform component to name the tf state file by', required=True)
+
+    return parser.parse_known_args()
+
+
+def validate_component(arguments):
+
+    if arguments.component == 'remote-state':
+        print('ERROR: Component cannot be one time remote state bucket!')
+        sys.exit(1)
 
 
 def switch_tf_version():
@@ -35,6 +51,7 @@ def init(arguments, path):
     file_contents = backend_config_file \
         .read() \
         .replace('{project}', arguments.project) \
+        .replace('{profile}', arguments.profile) \
         .replace('{account_id}', account_id) \
         .replace('{region}', arguments.region) \
         .replace('{environment}', arguments.environment) \
@@ -63,34 +80,32 @@ def get_account_id(arguments):
     return encoded_account_id.decode('utf-8').rstrip()
 
 
-def validate_component(arguments):
+def action(arguments, path, vars=None):
 
-    if arguments.component == 'remote-state':
-        print('ERROR: Component cannot be one time remote state bucket!')
-        sys.exit(1)
+    command = ['terraform', arguments.action]
+
+    if vars:
+        for var_string in vars:
+            command.append('-var')
+            command.append(var_string)
+
+    print(command)
+    execute(command, path, arguments)
 
 
-def parse_arguments():
+def execute(cmd, path, arguments):
 
-    parser = argparse.ArgumentParser()
+    # This env var provides a workaround for the following issue:
+    # https://github.com/hashicorp/terraform/issues/13589
+    env_vars = dict(os.environ, AWS_PROFILE=arguments.project)
 
-    parser.add_argument('--action', help='The Terraform action to perform e.g. plan', required=True)
-    parser.add_argument('--project', help='The name of the project default=azcard', default='azcard')
-    parser.add_argument('--region', help='The aws region default=eu-west-2', default='eu-west-2')
-    parser.add_argument('--environment', help='The aws environment e.g. test', required=True)
-    parser.add_argument('--component', help='The terraform component to name the tf state file by', required=True)
+    popen = subprocess.Popen(cmd,
+                             universal_newlines=True,
+                             cwd=path,
+                             env=env_vars)
 
-    return parser.parse_args()
+    popen.communicate(input="yes")
 
 
 if __name__ == '__main__':
-
-    args = parse_arguments()
-
-    validate_component(args)
-
-    component_path = os.path.join(BASE_PATH, 'components', args.component)
-
-    init(args, component_path)
-
-    execute(['terraform', args.action], component_path, args)
+    handle()
